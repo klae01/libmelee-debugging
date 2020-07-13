18b3b3acdc22dc2733c27a1e98e37a6b94b500c0
AltF4 <altf4petro@gmail.com>
2020-07-12 17:19:03 -0700

    Changed public API around console and controller creation  - separated consoles and controllers

--- example.py
+++ example.py
@@ -94,13 +94,7 @@ if args.live:
 #   Through this object, we can get "GameState" objects per-frame so that your
 #       bot can actually "see" what's happening in the game
 console = melee.console.Console(
-    ai_port=args.port,
-    is_dolphin=True,
-    opponent_port=args.opponent,
-    opponent_type=opponent_type,
-    dolphin_executable_path=args.dolphin_executable_path,
-    slippi_address=args.address,
-    logger=log,
+    path=args.dolphin_executable_path, slippi_address=args.address, logger=log
 )
 
 # Dolphin has an optional mode to not render the game's visuals
@@ -111,7 +105,13 @@ console.render = True
 #   The controller is the second primary object your bot will interact with
 #   Your controller is your way of sending button presses to the game, whether
 #   virtual or physical.
-controller = melee.controller.Controller(port=args.port, console=console)
+controller = melee.controller.Controller(
+    console=console, port=args.port, type=melee.enums.ControllerType.STANDARD
+)
+
+controller_opponent = melee.controller.Controller(
+    console=console, port=args.opponent, type=opponent_type
+)
 
 
 # This isn't necessary, but makes it so that Dolphin will get killed when you ^C
