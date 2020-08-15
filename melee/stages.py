e62f58433af8235475dc3f85e518673c674d1381
AltF4 <altf4petro@gmail.com>
2020-08-15 12:18:47 -0700

    Add extra helper function for side-platforms

--- melee/stages.py
+++ melee/stages.py
@@ -52,6 +52,22 @@ def top_platform_position(gamestate):
     return None
 
 
+def side_platform_position(right_platform, gamestate):
+    """Gets the position of the specified side platform
+
+    Args:
+        (bool): Is it the right platform?
+        (gamestate.GameState): The current GameState
+
+    Returns:
+        (float, float, float): Tuple of height, left edge, right edge
+    """
+    if right_platform:
+        return right_platform_position(gamestate)
+    else:
+        return left_platform_position(gamestate)
+
+
 def left_platform_position(gamestate):
     """Gets the position of the left platform
 
