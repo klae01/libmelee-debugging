471343116a26c6480465114ac611ef89bb0c69bb
AltF4 <altf4petro@gmail.com>
2020-09-16 21:40:42 -0700

    Fix bug with sometimes not generating gamestates on old SLP files

--- melee/console.py
+++ melee/console.py
@@ -455,6 +455,7 @@ class Console:
         playerstate.controller_state.button[enums.Button.BUTTON_D_UP] = bool(
             int(buttonbits) & 0x0008
         )
+        self._frame = gamestate.frame
 
     def __post_frame(self, gamestate, event_bytes):
         gamestate.stage = self._current_stage
@@ -632,6 +633,7 @@ class Console:
         except TypeError:
             ecb_right_y = 0
         playerstate.ecb_right = (ecb_right_x, ecb_right_y)
+        self._frame = gamestate.frame
 
     def __frame_bookend(self, gamestate, event_bytes):
         self._prev_gamestate = gamestate
