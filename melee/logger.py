1d8de32dfc76b090ad220d236f8f80f18030403e
AltF4 <altf4petro@gmail.com>
2021-03-08 14:54:10 -0700

    Add skipframe() function to logger, for easier-to-read logs

--- melee/logger.py
+++ melee/logger.py
@@ -112,3 +112,7 @@ class Logger:
         """Write the log to file"""
         self.writer.writeheader()
         self.writer.writerows(self.rows)
+
+    def skipframe(self):
+        """Skip this frame in the log"""
+        self.current_row = dict()
