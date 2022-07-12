5282c3b00ceac07506cfa8ace82812ae2499ed2f
AltF4 <altf4petro@gmail.com>
2022-07-11 21:45:47 -0700

    Add dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
         "numpy",
         'pywin32; platform_system=="Windows"',
         "packaging",
+        "pyserial",
     ],
     version="0.36.0",
     description="Open API written in Python 3 for making your own Smash Bros: Melee AI that works with Slippi Online",
